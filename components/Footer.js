958a91955f5c5a81c9b84ee165dd9d11b0d91abf
Henry <henryaccounts@yahoo.com>
2022-11-27 10:08:13 +0000

    updated readme

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        <p id="settingsText" onClick={ () => alert(1) }>Settings</p>
       </footer>
     </>
   )
